0c2b7bf9c46e22aa436d85238717a34db29fcefb
NIKHIL95969 <nikhilsingh95969@gmail.com>
2023-06-05 23:12:13 +0530

    todo app work done ui implemented

--- Todoapp/App.jsx
+++ Todoapp/App.jsx
@@ -1,176 +1,105 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  */
-
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Image,  TextInput, Button, FlatList, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([
+    { id: Date.now()+1, text: "Black & White Wireframe", completed: true },
+    { id: Date.now()+2, text: "User Research & Analysis", completed: true },
+    { id: Date.now()+3, text: "Design On Figma", completed: false },
+  ]);
+  const [todoText, setTodoText] = useState('');
 
-  const handleAddTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
+  const addTodo = () => {
+    if (todoText) {
+      setTodos(prevTodos => [...prevTodos, { id: Date.now(), text: todoText, completed: false }]);
+      setTodoText('');
     }
   };
 
-  const handleDeleteTodo = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
+  const toggleTodo = id => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
   };
 
+  const deleteAllTodo = () => {
+    setTodos([]);
+  }
+
+  const removeTodo = id => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  };
+
+  const renderTodo = ({ item }) => (
+    <View style={styles.todoItem}>
+      <TouchableOpacity
+        style={styles.checkbox}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.completed && <View style={styles.checkboxInnerCircle} >
+          <Text style={styles.checkboxMark}>✓</Text>
+        </View>}
+      </TouchableOpacity>
+
+      <Text style={item.completed ? styles.completedText : styles.todoText}>{item.text}</Text>
+      {/* <Button title="Delete" onPress={() => removeTodo(item.id)} /> */}
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Todo App</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.taskDetails}>
+        <Text style={styles.taskDetailsText}>Task Details</Text>
+      </View>
+      <View style={styles.headerContainer}>
+        <Text style={styles.taskTitle}>Task Title</Text>
+        <Text style={styles.titleText}>NFT Web App Prototype</Text>
 
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter a new todo"
-          value={newTodo}
-          onChangeText={(text) => setNewTodo(text)}
+        <Text style={styles.taskTitle}>Task Title</Text>
+        <Text style={styles.descriptionText}>Last year was a fantastic year for NFTs, with the market reaching a $40 billion valuation for the first time. In addition, more than $10 billion worth of NFTs are now sold every week – with NFT..
+        </Text>
+      </View>
+      <View style={styles.avatarView}>
+        <Image
+          source={require('./assets/images/Avatargp.jpg')} // Replace with the path or URL of your image
+          style={styles.image}
         />
-
-        <TouchableOpacity style={styles.addButton} onPress={handleAddTodo}>
-          <Text style={styles.addButtonText}>Add</Text>
+      </View>
+      <View style={styles.tasklistView}>
+        <Text style={styles.tasklist}>Task Title</Text>
+        <TouchableOpacity  onPress={deleteAllTodo}>
+          <Image
+              source={require('./assets/icons/Icon.jpg')} // Replace with the path or URL of your image
+              style={styles.imageDelete}
+            />
         </TouchableOpacity>
       </View>
-
-      {todos.map((todo, index) => (
-        <TouchableOpacity
-          key={index}
-          style={styles.todoItem}
-          onPress={() => handleDeleteTodo(index)}
-        >
-          <Text style={styles.todoText}>{todo}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
+      <FlatList
+        style={styles.todoList}
+        data={todos}
+        renderItem={renderTodo}
+        keyExtractor={item => item.id.toString()}
+      />
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputAddSign} >+</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Add Task"
+          placeholderTextColor="#98A2B3"
+          color="black"
+          value={todoText}
+          onChangeText={text => setTodoText(text)}
+          onSubmitEditing={addTodo}
+        />
+        {/* <Button title="Add" onPress={addTodo} /> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -178,49 +107,148 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 16,
   },
-  title: {
-    fontSize: 24,
+  taskDetails: {
+    flex: 1,
+    maxHeight: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  taskDetailsText: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#111322",
+  },
+  headerContainer: {
+    paddingHorizontal: 16,
+    // paddingVertical: 20,
+    // borderBottomWidth: 1,
+    // borderBottomColor: '#ccc',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 600,
+    color: "black",
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 15,
+  },
+  taskTitle: {
+    fontSize: 14,
+    fontWeight: 500,
+    color: "#5D6B98",
+    marginBottom: 12,
+  },
+  descriptionText: {
+    fontSize: 16,
+    color: "black",
+    marginTop: 4,
+    // marginBottom: 15,
+  },
+  avatarView: {
+    margin: 15, 
+    // flex: 1,
+    // flexDirection: 'row',
+    // justifyContent: 'space-between'
+  },
+  imageDelete: {
+    justifyContent: "center",
+    marginTop: 10,
+    marginRight: 10,
+    maxHeight: 20,
+  },
+  tasklistView: {
+    marginRight: 15,
+    flex: 1,
+    maxHeight: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tasklist: {
+   margin: 15,
+   color: "#5D6B98" 
   },
   inputContainer: {
     flexDirection: 'row',
-    marginBottom: 16,
+    alignItems: 'center',
+    backgroundColor: "#F9F9FB",
+    justifyContent: 'space-between',
+    borderRadius: 20,
+    margin: 15,
+    marginTop: 20,
+    marginBottom: 30,
+    padding: 10,
+  },
+  inputAddSign :{
+    fontSize: 30,
+    color: "#98A2B3",
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#F9F9FB",
   },
   input: {
     flex: 1,
-    borderWidth: 1,
+    marginRight: 10,
+    // borderWidth: 1,
+    placeholder: "Add Task",
+    // color: "Black",
+    fontSize: 16,
+    backgroundColor: "#F9F9FB",
     borderColor: '#ccc',
-    borderRadius: 4,
-    marginRight: 8,
-    color: "black",
-    paddingHorizontal: 8,
-    height: 40,
-  },
-  addButton: {
-    backgroundColor: 'blue',
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-    borderRadius: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  addButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
   },
   todoItem: {
-    backgroundColor: '#f2f2f2',
-    padding: 12,
-    color: "black",
-    marginBottom: 8,
-    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 5,
+    borderRadius: 10,
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    // borderTopLeftRadius: 3,
+    // borderTopRightRadius: 3,
+    // borderBottomLeftRadius: 3,
+    // borderBottomRightRadius: 3,
+    paddingLeft: 4,
+    paddingRight: 4,
+    flex: 0.3,
+    backgroundColor: '#F9F9FB',
+    // borderWidth: .1,
+  },
+  checkbox: {
+    width: 30,
+    height: 30,
+    borderWidth: 1,
+    borderColor: '#98A2B3',
+    borderRadius: 15,
+    alignItems: 'center',
+    marginRight: 10,
+    justifyContent: 'center',
+  },
+  checkboxInnerCircle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: '#7F56D9',
+  },
+  checkboxMark: {
+    textAlign: "center",
+    // paddingTop: 4,
+    fontSize: 20,
   },
   todoText: {
+    flex: 1,
+    marginRight: 10,
     fontSize: 16,
     color: "black",
+  },
+  completedText: {
+    flex: 1,
+    marginRight: 10,
+    // textDecorationLine: 'line-through',
+    color: "#98A2B3",
+    // color: '#ccc',
   },
 });
 
